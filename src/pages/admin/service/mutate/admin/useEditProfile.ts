f4c4fbabd333535ae271f805fa4e060688e45544
suhrobswe <suhrobabdurazzok@gmail.com>
2026-01-15 15:35:41 +0500

    v1

--- src/pages/admin/service/mutate/admin/useEditProfile.ts
+++ src/pages/admin/service/mutate/admin/useEditProfile.ts
@@ -2,7 +2,7 @@ import { request } from "@/config/request";
 import type { ChangePassword, EditProfile } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
-export const useEditProfile = (p0: { username: string; phone: string; }) => {
+export const useEditProfile = (_p0: { username: string; phone: string; }) => {
     return useMutation({
         mutationFn: (data: EditProfile) => request.patch("/admin/update", data),
     });
